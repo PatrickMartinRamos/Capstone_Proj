1a4bcf5365cdf2238e5bbaa20ef25562c0d4e404
Angelik Operio <122664874+LalaFC@users.noreply.github.com>
2025-11-24 07:41:03 +0800

    Latest update

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -13,6 +13,7 @@ public class AudioManager : MonoBehaviour
     {
         if (Instance != null)
         {
+            Debug.Log("Destroying "+this.gameObject.name);
             Destroy(gameObject);
             return;
         }
